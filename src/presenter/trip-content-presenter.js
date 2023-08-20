7b6ba85b366f871ee6a4c4acf96c7fb06852d4e3
Deimon <deimon1993rf2@yandex.ru>
2023-08-20 20:08:57 +0300

    Переводим модель и презентеры на привытные свойства/методы

--- src/presenter/trip-content-presenter.js
+++ src/presenter/trip-content-presenter.js
@@ -8,22 +8,32 @@ import EditPointView from '../view/edit-point-view.js';
 import PointsModel from '../model/points-model.js';
 
 export default class TripContentPresenter {
+  #tripEventsContainer = null;
+  #tripEventsListContainer = null;
+  #pointsModel = null;
+  #points = null;
+
   constructor() {
-    this.tripEventsContainer = document.querySelector('.trip-events'); // Общий контейнер для событий
-    this.tripEventsListContainer = new TripEventsList(); // Контейнер для списка точек маршрута
-    this.pointsModel = new PointsModel(); // Модель для получения данных о точках маршрута
+    this.#tripEventsContainer = document.querySelector('.trip-events'); // Общий контейнер для событий
+    this.#tripEventsListContainer = new TripEventsList(); // Контейнер для списка точек маршрута
+    this.#pointsModel = new PointsModel(); // Модель для получения данных о точках маршрута
   }
 
   init() {
-    this.points = this.pointsModel.getPoints().slice(); // Копируем полученный из модели массив с точками маршрута
+    this.#points = this.#pointsModel.points.slice(); // Копируем полученный из модели массив с точками маршрута
 
-    render(new TripSortView(), this.tripEventsContainer); // Отрисовываем сортировку событий
-    render(this.tripEventsListContainer, this.tripEventsContainer); // Отрисовываем контейнер для событий
-    render(new EditPointView(this.points[0]), this.tripEventsListContainer.element, RenderPosition.AFTERBEGIN); // Отрисовываем форму редактирования первой точки маршрута
-    // render(new EditPointView(), this.tripEventsListContainer.getElement(), RenderPosition.AFTERBEGIN); // Форма создания новой точки (форма редактирования с пустым конструктором)
+    render(new TripSortView(), this.#tripEventsContainer); // Отрисовываем сортировку событий
+    render(this.#tripEventsListContainer, this.#tripEventsContainer); // Отрисовываем контейнер для событий
+    render(new EditPointView(this.#points[0]), this.#tripEventsListContainer.element, RenderPosition.AFTERBEGIN); // Отрисовываем форму редактирования первой точки маршрута
 
-    for (let i = 1; i < this.points.length; i++) { //Выводим не с первой точки, а со второй т.к. первая отводится под блок редактирования
-      render(new TripEventsListItem(this.points[i]), this.tripEventsListContainer.element); // Отрисовываем события
+    for (let i = 1; i < this.#points.length; i++) { //Выводим не с первой точки, а со второй т.к. первая отводится под блок редактирования
+      this.#renderEventPoint(this.#points[i]); // Отрисовываем события; // Отрисовываем события
     }
+  }
+
+  #renderEventPoint(point) {
+    const pointComponent = new TripEventsListItem(point);
+
+    render(pointComponent, this.#tripEventsListContainer.element);
   }
 }
