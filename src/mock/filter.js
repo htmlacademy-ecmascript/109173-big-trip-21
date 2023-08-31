f6e09d918686c24f1b98215b9b8c6d2d739a5750
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-08-31 18:09:46 +0300

    Добавляем в модель методы для установки и получени фильтров (модуль 7)

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -2,16 +2,28 @@ import { FilterType, filters } from '../utils/filters.js';
 
 const DEFAULT_FILTER = FilterType.EVERYTHING;
 
-function createFilters(points) {
-  return Object.entries(filters).map(([filterName, filterFunc]) => {
+function createFilters() {
+  return filters.map((filterName) => {
     const checkedState = filterName === DEFAULT_FILTER ? 'checked' : '';
 
     return {
       name: filterName,
       checked: checkedState,
-      dataLength: points ? filterFunc(points).length : 0,
     };
   });
 }
+
+// Если вдруг понадобится выводить количество точек, соответствующих фильтру
+// function createFilters(points) {
+//   return Object.entries(filters).map(([filterName, filterFunc]) => {
+//     const checkedState = filterName === DEFAULT_FILTER ? 'checked' : '';
+
+//     return {
+//       name: filterName,
+//       checked: checkedState,
+//       dataLength: points ? filterFunc(points).length : 0,
+//     };
+//   });
+// }
 
 export { createFilters };
--- src/model/filters-model.js
+++ src/model/filters-model.js
@@ -1,9 +1,20 @@
 import { createFilters } from '../mock/filter.js';
+import { FilterType } from '../utils/filters';
 
+const DEFAULT_FILTER = FilterType.EVERYTHING;
 export default class FiltersModel {
+  #currentFilter = FilterType[DEFAULT_FILTER];
   #filters = createFilters();
 
   get filters() {
     return this.#filters;
+  }
+
+  set filter(filterType) {
+    this.#currentFilter = filterType;
+  }
+
+  get filter() {
+    return FilterType[this.#currentFilter];
   }
 }
