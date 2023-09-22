7e8a656029f35b6a2c430703eb4e898cd3c4541a
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-09-22 18:21:59 +0300

    Реализация обновления точки WIP

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -1,5 +1,6 @@
+import Adapter from './adapter.js';
 import ApiService from './framework/api-service.js';
-import { API_URL, AUTH_TOKEN, END_POINT } from './utils/const.js';
+import { API_URL, AUTH_TOKEN, END_POINT, Method } from './utils/const.js';
 
 
 export default class PointsApiService extends ApiService {
@@ -10,5 +11,18 @@ export default class PointsApiService extends ApiService {
   get points() {
     return this._load({ url: API_URL.POINTS })
       .then(ApiService.parseResponse);
+  }
+
+  async updatePoint(updatedPoint) {
+    const adaptedPoint = Adapter.adaptPointToServer(updatedPoint);
+    const response = await this._load({
+      url: `${API_URL.POINTS}/${updatedPoint.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(adaptedPoint),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
   }
 }
